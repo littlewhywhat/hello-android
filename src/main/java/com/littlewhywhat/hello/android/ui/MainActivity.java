ab3840af3c71369f5c44aec37c9aeb1c00adfa20
littlewhywhat <littlewhywhat@gmail.com>
2015-06-29 23:55:13 +0300

    added main.xml 	MainActivity: imported R class, 		      onCreate: changed using R, 		      getHello: added; 	main.xml: added

--- src/main/java/com/littlewhywhat/hello/android/ui/MainActivity.java
+++ src/main/java/com/littlewhywhat/hello/android/ui/MainActivity.java
@@ -1,8 +1,9 @@
 package com.littlewhywhat.hello.android.ui;
 
+import com.littlewhywhat.hello.android.R;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -11,10 +12,11 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		final RelativeLayout layout = new RelativeLayout(this);
-		final TextView textView = new TextView(this);
-		textView.setText(HELLO_ANDROID);
-		layout.addView(textView);
-		this.setContentView(layout);
+		this.setContentView(R.layout.main);
+		getHello().setText(HELLO_ANDROID);
+	}
+
+	private TextView getHello() {
+		return (TextView)findViewById(R.id.hello);
 	}
 }
